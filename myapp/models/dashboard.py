2339606bfa5d7bcb07ba539efdd3f31041ec2b0c
Dorothy <kizdorothy@gmail>
2017-11-03 17:04:10 +0300

    Fix error due to unhashed password

--- myapp/models/dashboard.py
+++ myapp/models/dashboard.py
@@ -1,36 +1,31 @@
-from passlib.apps import custom_app_context as pwd_context
+from passlib.hash import pbkdf2_sha256  # from the passlib docs
 from myapp.models.user import User
-"""
-a dashboard class that enables  the user to sign in
-and log out and register at once
-     they are on the app.
-"""
 
 
 class Dashboard:
     def __init__(self):
-        self.registry = dict()  # list registry
+        self.registry = dict()
         self.is_logged_in = False
 
     def signup(self, name, email, password):
         has_been_registered = False
-        user = User(name, email, password)
+        user = User(name, email, pbkdf2_sha256.hash(password))
         if user and user.email not in self.registry:
             self.registry[user.email] = user
             has_been_registered = True
         return has_been_registered
 
     def login(self, email, password):
-        if email not in self.registry:
+        if email not in self.registry:  # if user is not yet registered...
             return False
-        if pwd_context.verify(password, self.registry[email].password):
+
+        if pbkdf2_sha256.verify(password, self.registry[email].password):
             self.is_logged_in = True
             return True
         return False
 
     def logout(self):
         is_logged_out = False
-        if self.is_logged_in:
-
+        if self.is_logged_in:  # for a user to logout, they must be logged in
             is_logged_out = True
         return is_logged_out
